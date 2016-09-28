555afc3da0d05901ec7ca477f6977fa919a4c3a2
Tobias <tobpi009@student.liu.se>
2016-09-28 15:53:22 +0200

    added fish prefab with speed and random rotation, also food prefab

--- TNM095 Evolution AI/Assets/scripts/movingObject.cs
+++ TNM095 Evolution AI/Assets/scripts/movingObject.cs
@@ -8,18 +8,25 @@ public class movingObject : MonoBehaviour {
 	public int visabilityRange;
 
 	private Vector2 forwardDirection;
+	private int foodCounter = 0;
 
 	// Use this for initialization
 	void Start () {
 
 		//Randomize inital forward direction
-		float x = Random.Range(-1f, 1f);
-		float y = Random.Range(-1f, 1f);
-		forwardDirection = new Vector2(x, y);
+//		float x = Random.Range(-1f, 1f);
+//		float y = Random.Range(-1f, 1f);
+		forwardDirection = new Vector2(1, 1);
+		forwardDirection.Normalize ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		//Update forward direction with a random angle within the units maximum turnAngle
+		float randomAngle = Random.Range(-turnAngle, turnAngle);
+		forwardDirection = Quaternion.Euler (0, 0, randomAngle) * forwardDirection;
+		forwardDirection.Normalize();
 
 		//Move individual forward with constant speed
 		transform.Translate(forwardDirection * Time.deltaTime * speed);
