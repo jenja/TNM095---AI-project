65f4a3626eda1d02db459c336be25d54c52ee846
Jens Jakobsson <jens.j.jakobsson@gmail.com>
2016-09-28 14:27:51 +0200

    created a DNA class

---
+++ TNM095 Evolution AI/Assets/scripts/DNA.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DNA : MonoBehaviour {
+
+    //Attributes
+    public float speed { get; set; }
+    public float turnAngle { get; set; }
+    public float visRange { get; set; }
+
+    //Constructor
+    public DNA() {
+        speed = 0;
+        turnAngle = 0;
+        visRange = 0;
+    }
+}
