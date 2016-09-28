d741d22cbf87a97590b89b0079e1968d9837bdc3
Tobias <tobpi009@student.liu.se>
2016-09-28 14:49:25 +0200

    added a script for moving the fish and created a fish prefab

---
+++ TNM095 Evolution AI/Assets/scripts/movingObject.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movingObject : MonoBehaviour {
+
+	public int speed;
+	public int turnAngle;
+	public int visabilityRange;
+
+	private Vector2 forwardDirection;
+
+	// Use this for initialization
+	void Start () {
+
+		//Randomize inital forward direction
+		float x = Random.Range(-1f, 1f);
+		float y = Random.Range(-1f, 1f);
+		forwardDirection = new Vector2(x, y);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Move individual forward with constant speed
+		transform.Translate(forwardDirection * Time.deltaTime * speed);
+	}
+}
